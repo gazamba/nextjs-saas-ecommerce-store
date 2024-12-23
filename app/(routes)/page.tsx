8b4c018d3e9d976f86d28937258d250dd8c9324f
I504428 <gabriel.azambuja@sap.com>
2024-12-23 15:04:53 -0300

    feat: style home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Container from "../components/ui/Container";
+import Billboard from "../components/Billboard";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "../components/ProductList";
 
-const HomePage = () => {
-  return <div>Hello Store.</div>;
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("a35c9662-5c52-4898-8ea2-df17e4d1658d");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
