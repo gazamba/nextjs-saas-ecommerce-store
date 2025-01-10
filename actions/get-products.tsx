ba960a30e83275f6d38732861e1fc42ce7c32c19
I504428 <gabriel.azambuja@sap.com>
2025-01-10 03:30:24 -0300

    feat: add actions

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -14,12 +14,15 @@ const getProducts = async (query: Query): Promise<Product[]> => {
   const url = qs.stringifyUrl({
     url: URL,
     query: {
-      colorId: query.categoryId,
+      colorId: query.colorId,
       sizeId: query.sizeId,
       categoryId: query.categoryId,
       isFeatured: query.isFeatured,
     },
   });
+  console.log("Query object:", query);
+
+  console.log("Constructed URL:", url);
 
   const res = await fetch(url);
 
