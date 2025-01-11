f3695288e0bc4e65168849e7556f97f52e7104f4
I504428 <gabriel.azambuja@sap.com>
2025-01-10 22:25:01 -0300

    fix: update currency from div to span to fix hydration issue

--- app/components/ui/Currency.tsx
+++ app/components/ui/Currency.tsx
@@ -16,7 +16,9 @@ const Currency = ({ value }: CurrencyProps) => {
 
   if (!isMounted) return null;
 
-  return <div className="font-semibold">{formatter.format(Number(value))}</div>;
+  return (
+    <span className="font-semibold">{formatter.format(Number(value))}</span>
+  );
 };
 
 export default Currency;
