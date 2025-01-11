5352283ab9c85e3cea280f0d3c30e7bb9905c616
I504428 <gabriel.azambuja@sap.com>
2025-01-10 22:25:47 -0300

    feat: add info section to product page

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
 import Gallery from "@/app/components/gallery";
+import Info from "@/app/components/Info";
 import ProductList from "@/app/components/ProductList";
 import Container from "@/app/components/ui/Container";
 import React from "react";
@@ -15,15 +16,15 @@ const ProductPage = async ({ params }: ProductPageProps) => {
     categoryId: product?.category?.id,
   });
 
-  console.log(suggestedProducts);
-
   return (
     <div className="bg-white">
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             <Gallery images={product.images} />
-            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">Info</div>
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <Info data={product} />
+            </div>
           </div>
           <hr className="my-10" />
           <ProductList title="Related Products" items={suggestedProducts} />
