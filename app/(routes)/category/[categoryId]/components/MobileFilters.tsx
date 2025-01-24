3c3838980d1f8ccf93938c93234e603653fc1233
I504428 <gabriel.azambuja@sap.com>
2025-01-24 20:24:28 -0300

    feat: add mobile filters

---
+++ app/(routes)/category/[categoryId]/components/MobileFilters.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Button from "@/app/components/ui/Button";
+import IconButton from "@/app/components/ui/IconButton";
+import { Color, Size } from "@/types";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Plus, X } from "lucide-react";
+import { useState } from "react";
+import Filter from "./Filter";
+
+interface MobileFiltersProps {
+  sizes: Size[];
+  colors: Color[];
+}
+
+const MobileFilters = ({ sizes, colors }: MobileFiltersProps) => {
+  const [open, setOpen] = useState(false);
+
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(false);
+
+  return (
+    <>
+      <Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden">
+        Filters
+        <Plus size={20} />
+      </Button>
+
+      <Dialog
+        open={open}
+        as="div"
+        className="relative z-40 lg:hidden"
+        onClose={onClose}
+      >
+        <div className="fixed inset-0 bg-black bg-opacity-25" />
+
+        <div className="fixed inset-0 flex w-screen items-center justify-center">
+          <DialogPanel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+            <div className="flex items-center justify-end px-4">
+              <IconButton onClick={onClose} icon={<X size={15} />} />
+            </div>
+
+            <div className="p-4">
+              <Filter valueKey="sizeId" name="Size" data={sizes} />
+              <Filter valueKey="colorId" name="Color" data={colors} />
+            </div>
+          </DialogPanel>
+        </div>
+
+        {/* <div className="fixed inset-o z-40 flex">
+          <DialogPanel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+            <div className="flex items-center justify-end px-4">
+              <IconButton onClick={onClose} icon={<X size={15} />} />
+            </div>
+
+            <div className="p-4">
+              <Filter valueKey="sizeId" name="Size" data={sizes} />
+              <Filter valueKey="colorId" name="Color" data={colors} />
+            </div>
+          </DialogPanel>
+        </div> */}
+      </Dialog>
+    </>
+  );
+};
+
+export default MobileFilters;
