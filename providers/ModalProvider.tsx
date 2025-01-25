abdf59b386e546b1bb9328da35f395eccc9e06a4
I504428 <gabriel.azambuja@sap.com>
2025-01-24 23:42:54 -0300

    feat: add modal provider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import PreviewModal from "@/app/components/PreviewModal";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return <PreviewModal />;
+};
+
+export default ModalProvider;
