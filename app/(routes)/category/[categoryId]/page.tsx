8fca52a709478c70438c730320a8ede99eec1865
I504428 <gabriel.azambuja@sap.com>
2025-01-24 21:23:50 -0300

    feat: updating page

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -6,6 +6,9 @@ import Billboard from "@/app/components/Billboard";
 import Container from "@/app/components/ui/Container";
 import React from "react";
 import Filter from "./components/Filter";
+import NoResults from "@/app/components/ui/NoResults";
+import ProductCard from "@/app/components/ui/ProductCard";
+import MobileFilters from "./components/MobileFilters";
 
 export const revalidate = 0;
 
@@ -34,10 +37,18 @@ const CategoryPage = async ({ params, searchParams }: CategoryPageProps) => {
         <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-            {/* Add mobile filters */}
+            <MobileFilters sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Size" data={sizes} />
               <Filter valueKey="colorId" name="Color" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
