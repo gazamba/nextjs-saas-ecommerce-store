6a17f63b12d64b101b7c6031cbb3063967a10c50
I504428 <gabriel.azambuja@sap.com>
2025-01-25 21:53:25 -0300

    feat: add summary section

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Container from "@/app/components/ui/Container";
 import useCart from "@/hooks/useCart";
 import CartItem from "./components/CartItem";
+import Summary from "./components/Summary";
 
 const CartPage = () => {
   const cart = useCart();
@@ -20,7 +21,7 @@ const CartPage = () => {
       <Container>
         <div className="px-4 py-16 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
-          <div className="mt-12 lg:grid lg:grid=cols-12 lg:items-start gap-x-12">
+          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
               {cart.items.length === 0 && (
                 <p className="text-neutral-500">No items added to cart.</p>
@@ -31,6 +32,7 @@ const CartPage = () => {
                 ))}
               </ul>
             </div>
+            <Summary />
           </div>
         </div>
       </Container>
